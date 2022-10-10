e2679404dade1e28f156dc89a54316f03062375e
coolian1337 <julian.wogersien@gmail.com>
2022-10-10 12:40:12 +0200

    working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hexWriter
-Writes hex to a file
+This program takes a file name or path as an input and writes the hex data inside it to a file as actual bytes
+Here is an example of how the File needs to be formatted:\
+0x45, 0x27, 0xff, 0x4c
+
+
+Anything that is not a valid character will be ignored except commas.
+commas are used to seperate each hex number.
+
+## Contributing
+Create a PR
--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <ctype.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 #ifdef WINDOWS
 #include <direct.h>
@@ -14,100 +14,127 @@
 #endif
 
 uint64_t bufSize = 32768;
+const char validChars[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+                           'B', 'C', 'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-int exists(const char*);
-char* loadFile(const char*);
-char* processBuffer(char* buf);
-unsigned char *convert(const char* s, int* length);
-static unsigned char gethex(const char* s, char** endptr);
-void writeFile(const char* fileName, char* buf);
+int exists(const char *);
+char *loadFile(const char *);
+char *processBuffer(char *buf);
+unsigned char *convert(const char *s, int *length);
+static unsigned char gethex(const char *s, char **endptr);
+void writeFile(const char *fileName, char *buf);
 
-int exists(const char* file)
+int exists(const char *file)
 {
-    if(access(file, F_OK) == 0)
+    if (access(file, F_OK) == 0)
     {
         return 1;
     }
     return 0;
 }
 
-char* loadFile(const char* file)
+char *loadFile(const char *file)
 {
-    char* buf = (char*) malloc(bufSize);
-    FILE* fp = fopen(file, "r");
-
+    char *buf = (char *)calloc(bufSize, sizeof(char));
+    FILE *fp = fopen(file, "r");
+    char c;
     int counter = 0;
-    char ch = ' ';
-    while((ch = (char)fgetc(fp)) != EOF)
+    if (fp == NULL)
     {
-        buf[counter] = ch;
-        counter++;
+        printf("error could not open file");
+        exit(1);
     }
-    buf[counter] = EOF;
+    do
+    {
+        c = (char)fgetc(fp);
+        if (c == EOF)
+        {
+            if (buf[counter - 1] != ',')
+            {
+                buf[counter] = ',';
+                counter++;
+            }
+            buf[counter] = EOF;
+            break;
+        }
+        buf[counter] = c;
+        counter++;
+    } while (1);
+
+    char *rsbuf = (char *)calloc((size_t)counter, sizeof(char));
+    memcpy(rsbuf, buf, (size_t)counter);
+    bufSize = (uint64_t)counter;
+
     fclose(fp);
-    return buf;
+    return (char *)buf;
 }
 
-static unsigned char gethex(const char* s, char** endptr) {
-    while (isspace(*s)) s++;
+static unsigned char gethex(const char *s, char **endptr)
+{
+    while (isspace(*s))
+        s++;
     return (unsigned char)strtoul(s, endptr, 16);
 }
 
-unsigned char* convert(const char* s, int* length) {
-    unsigned char* answer = malloc((strlen(s) + 1) / 3);
-    unsigned char* p;
+unsigned char *convert(const char *s, int *length)
+{
+    unsigned char *answer = malloc((strlen(s) + 1) / 3);
+    unsigned char *p;
     for (p = answer; *s; p++)
-        *p = gethex(s, (char**)&s);
+        *p = gethex(s, (char **)&s);
     *length = (int)(p - answer);
     return answer;
 }
 
-char* processBuffer(char* buf)
+char *processBuffer(char *buf)
 {
-    char* out = (char*)calloc(bufSize, sizeof(char));
-    char* cht = calloc(16, sizeof(char));
-    char ch;
-    char done = 0;
-    int count = 0;
-    int chCount = 0;
-    while(!done)
+    char *out = (char *)calloc(bufSize, sizeof(char));
+    char *ccHex = calloc(16, sizeof(char));
+    int hexCount = 0;
+    int ccCount = 0;
+    for (int i = 0; i < (int)bufSize; i++)
     {
-        ch = buf[count];
-        if(ch == ',' || ch == '.')
+        char *c = &buf[i];
+        printf("%c", *c);
+        int valid = 0;
+        for (int j = 0; j < 21; j++)
         {
-            out[count] = (char)*convert(cht, &chCount);
-            chCount = 0;
-        }else if(ch == EOF)
-        {
-            break;
-        }else if(ch == ' ')
-        {
-        }else
-        {
-            cht[chCount] = ch;
-            chCount++;
+            if (*c == validChars[j])
+                valid = 1;
         }
-        count++;
+        if (valid)
+        {
+            ccHex[ccCount] = *c;
+            ccCount++;
+        }
+        else if (*c == ',')
+        {
+            out[hexCount] = (char)*convert(ccHex, &ccCount);
+            hexCount++;
+            ccCount = 0;
+        }
     }
-    count++;
-    out[count] = EOF;
+    out[hexCount] = EOF;
     free(buf);
     return out;
 }
 
-void writeFile(const char* fileName, char* buf)
+void writeFile(const char *fileName, char *buf)
 {
-    FILE* fp = fopen(fileName, "w");
+    FILE *fp = fopen(fileName, "wb");
 
     char done = 0;
     int count = 0;
-    while(!done)
+    while (!done)
     {
-        if(buf[count] != 0x0)
-            putc(buf[count], fp);
-        if(buf[count] == EOF)
+        if (buf[count] == EOF)
         {
             done = 1;
+        }
+        else
+        {
+            if (buf[count] != 0x0)
+                putc(buf[count], fp);
         }
         count++;
     }
@@ -115,26 +142,29 @@ void writeFile(const char* fileName, char* buf)
 
 int main(int argc, char *argv[])
 {
-    char* outName;
+    char *outName;
 
-    if(argc < 2) exit(1);
+    if (argc < 2)
+        exit(1);
 
-    char* file = argv[1];
+    char *file = argv[1];
 
-    if(argc >= 3){
+    if (argc >= 3)
+    {
         bufSize = (uint64_t)atoi(argv[2]);
     }
 
     outName = calloc(strlen(file) + 4, sizeof(char));
     strcat(outName, file);
-    strcat(outName, ".gz");
+    strcat(outName, ".hex");
 
-    if(!exists(file)){
+    if (!exists(file))
+    {
         printf("file doesnt exist  %s\n", file);
         exit(1);
     }
 
-    char* buf = loadFile(file);
+    char *buf = loadFile(file);
     buf = processBuffer(buf);
     writeFile(outName, buf);
     free(buf);
